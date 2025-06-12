5038a87fc9acbec046db9feb4565a603db38f462
Jonas Fechner <i40014635@pcm.endress.com>
2025-06-12 14:57:08 +0200

    Added 5 more exercise

--- export_onnx_model.py
+++ export_onnx_model.py
@@ -2,9 +2,9 @@ import torch
 from model import PoseBinaryPT
 
 # -------------------- Configuration --------------------
-exercise = 'push_up'
-model_path = f"weights/{exercise}_binary_best_norm.pth"
-onnx_output_path = f"weights/{exercise}_model.onnx"
+exercise = 'front_raise'
+model_path = f"weights/torch/{exercise}_binary_best_norm.pth"
+onnx_output_path = f"weights/onnx/{exercise}_model.onnx"
 dim = 26  # Number of input features (keypoints)
 dummy_input = torch.randn(1, 1, dim)  # [batch=1, seq_len=1, features=26]
 
@@ -12,7 +12,7 @@ dummy_input = torch.randn(1, 1, dim)  # [batch=1, seq_len=1, features=26]
 model = PoseBinaryPT(dim=dim, heads=2, enc_layers=1, alpha=0.5)
 model.load_state_dict(torch.load(model_path, map_location="cpu"))
 model.eval()
-print("✅ Model loaded")
+print("Model loaded")
 
 # -------------------- Export to ONNX --------------------
 torch.onnx.export(
@@ -26,4 +26,4 @@ torch.onnx.export(
     output_names=["logits", "features"]
 )
 
-print(f"✅ ONNX model exported to: {onnx_output_path}")
+print(f"ONNX model exported to: {onnx_output_path}")
